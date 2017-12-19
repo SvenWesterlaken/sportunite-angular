9c6f1af113dfd30a9e400174a273a9fcf15bf05e
SVEN-LAPTOP\Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-12-19 15:27:38 +0100

    Setup for authentication with auth service & some basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import {AuthService} from './services/auth.service';
+import { RegisterComponent } from './components/auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
