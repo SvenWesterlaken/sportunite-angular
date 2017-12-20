e7ccbe1d48bcc39637ac612b0d283464c174e584
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2017-12-20 20:52:08 +0100

    Added button styling & Added some error handling with the login

--- src/app/components/auth/login/login.component.ts
+++ src/app/components/auth/login/login.component.ts
@@ -3,6 +3,7 @@ import {AuthService} from '../../../services/auth.service';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {CustomErrorStateMatcher} from '../../../other/errorstate.manager';
 import {Router} from '@angular/router';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -27,6 +28,20 @@ export class LoginComponent implements OnInit {
   submit() {
     this.auth.login(this.loginForm.value).then(() => {
       this.router.navigate(['/']);
+    }).catch((res: HttpErrorResponse) => {
+      if (res.status === 0) {
+        this.loginForm.setErrors({
+          'noConnection': true
+        });
+      } else if (res.status === 404) {
+        this.loginForm.get('email').setErrors({
+          'notFound': true
+        });
+      } else if (res.status === 401) {
+        this.loginForm.get('password').setErrors({
+          'invalidPassword': true
+        });
+      }
     });
   }
 
