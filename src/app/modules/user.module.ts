b12a41293c9cd4dcd26a9dc35397a2b3164b0901
SVEN-LAPTOP\Sven Westerlaken <sven.westerlaken@hotmail.nl>
2018-01-15 14:45:37 +0100

    Quick fix

--- src/app/modules/user.module.ts
+++ src/app/modules/user.module.ts
@@ -1,22 +1,19 @@
 import {NgModule} from '@angular/core';
 import {SharedModule} from './shared.module';
-import {ReactiveFormsModule} from "@angular/forms";
-import {FormsModule} from "@angular/forms";
-import {MatChipsModule, MatInputModule, MatProgressSpinnerModule, MatSnackBarModule} from "@angular/material";
-import {MatSelectModule, MatCardModule} from "@angular/material";
-import {MatTooltipModule} from "@angular/material";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {HttpModule} from "@angular/http";
-import {StepperModule} from "./stepper.module";
-import {DatepickerModule} from "./datepicker.module";
-import {UserDetailComponent} from "../components/user/user-detail/user-detail.component";
-import {UserEditComponent} from "../components/user/user-edit/user-edit.component";
-import {UserComponent} from "../components/user/user.component";
-import {UserRoutingModule} from "../routes/user.routing.module";
-import {UserService} from "../services/user.service";
-import {UserDialogComponent} from "../components/user/user-edit/user-dialog/user-dialog.component";
-import {SuiModule} from 'ng2-semantic-ui';
-import {MomentModule} from "angular2-moment";
+import {ReactiveFormsModule} from '@angular/forms';
+import {
+  MatCardModule, MatChipsModule, MatInputModule, MatProgressSpinnerModule, MatSelectModule, MatSnackBarModule,
+  MatTooltipModule
+} from '@angular/material';
+import {StepperModule} from './stepper.module';
+import {DatepickerModule} from './datepicker.module';
+import {UserDetailComponent} from '../components/user/user-detail/user-detail.component';
+import {UserEditComponent} from '../components/user/user-edit/user-edit.component';
+import {UserComponent} from '../components/user/user.component';
+import {UserRoutingModule} from '../routes/user.routing.module';
+import {UserService} from '../services/user.service';
+import {UserDialogComponent} from '../components/user/user-edit/user-dialog/user-dialog.component';
+import {MomentModule} from 'angular2-moment';
 
 @NgModule({
     entryComponents: [
