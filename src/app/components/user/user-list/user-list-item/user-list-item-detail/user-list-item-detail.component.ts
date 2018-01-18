b18a7885ed828d51e9e397be2c59677983043251
Maikel <maikel_j@live.nl>
2018-01-18 14:38:13 +0100

    Showing friends->friends relationship

--- src/app/components/user/user-list/user-list-item/user-list-item-detail/user-list-item-detail.component.ts
+++ src/app/components/user/user-list/user-list-item/user-list-item-detail/user-list-item-detail.component.ts
@@ -19,6 +19,8 @@ export class UserListItemDetailComponent implements OnInit, OnDestroy {
     friendsWith: boolean;
     sportEvents: SportEvent[];
     private friendSub: Subscription;
+    private friends: User[];
+
 
     constructor(private route: ActivatedRoute, private router: Router,
                 private userService: UserService, private eventService: EventService) {
@@ -47,12 +49,14 @@ export class UserListItemDetailComponent implements OnInit, OnDestroy {
                 if (!this.user) {
                     this.userService.getUsers(this.id).then((user: User) => {
                         this.user = user;
+                        this.userService.getUserFriends(this.user._id).then((friends: User[]) => this.friends = friends);
                         this.friendsWith = friends.some(user => user._id === this.user._id);
                         if (this.router.isActive('/friends', false) && !this.friendsWith) {
                             this.router.navigate(['..'], {relativeTo: this.route});
                         }
                     });
                 } else {
+                    this.userService.getUserFriends(this.user._id).then((friends: User[]) => this.friends = friends);
                     this.friendsWith = friends.some(user => user._id === this.user._id);
                 }
             });
